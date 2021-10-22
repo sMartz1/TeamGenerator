43f205e5e1b59047c417e98af95143ebf2ff8d87
Sebastian M <ra3gan1991@gmail.com>
2021-10-22 09:51:59 +0200

    Creo funcion para generar lista por numero de participantes o por equipos totales.

--- script/main.js
+++ script/main.js
@@ -79,7 +79,7 @@ function changeDom() {
 let valores = document.getElementsByClassName("text");
 //De esta manera detectamos el evento y lo recogemos en e para comprobar el key code
 //Aqui el Enter en este elemento añadira un nuevo player
-valores[0].addEventListener("keydown", function (e) {
+valores[0].addEventListener("keydown", function(e) {
     if (e.keyCode === 13) {
         addPlayer();
     }
@@ -97,20 +97,20 @@ function getPlayersByFile(e) {
     //Usamos file reader para acceder al archivo
     let reader = new FileReader();
     //Un controlador para el evento load. Este evento se activa cada vez que la operación de lectura se ha completado satisfactoriamente.
-    reader.onload = function (e) {
-        //en .result encontramos la string resultante de leer el archivo.
-        let content = e.target.result;
-        //Parto por saltos de linea los nombres para recibirlos como array
-        let arrGenerado = content.split(/\r?\n/g);
-        //Tenemos la variable arrGenerado con los players del .txt para su futuro uso.
-        //De momento dejamos en log el array
-        for (let i = 0; i < arrGenerado.length; i++) {
-            addPlayer(arrGenerado[i]);
+    reader.onload = function(e) {
+            //en .result encontramos la string resultante de leer el archivo.
+            let content = e.target.result;
+            //Parto por saltos de linea los nombres para recibirlos como array
+            let arrGenerado = content.split(/\r?\n/g);
+            //Tenemos la variable arrGenerado con los players del .txt para su futuro uso.
+            //De momento dejamos en log el array
+            for (let i = 0; i < arrGenerado.length; i++) {
+                addPlayer(arrGenerado[i]);
+
+            }
 
         }
-
-    }
-    //Se le pasa archivo a leer, si lo lee correctamente haria trigger en el .onload definido anteriormente.
+        //Se le pasa archivo a leer, si lo lee correctamente haria trigger en el .onload definido anteriormente.
     reader.readAsText(file);
     document.getElementById("inputF").value = "";
 }
@@ -185,7 +185,7 @@ function modifyPlayer(player) {
         if (player.isContentEditable) {
 
             //Evento que detectara si el usuario pulsa ENTER
-            player.addEventListener("keydown", function (e) {
+            player.addEventListener("keydown", function(e) {
                 if (e.keyCode === 13) {
                     player.removeAttribute("contentEditable")
                     valor = player.textContent;
@@ -194,7 +194,7 @@ function modifyPlayer(player) {
             })
 
             //Evento que controlara el valor si el elemento pierde el foco
-            player.addEventListener('blur', function (e) {
+            player.addEventListener('blur', function(e) {
                 player.addEventListener
                 player.removeAttribute("contentEditable")
                 valor = player.textContent;
@@ -238,4 +238,50 @@ function tipUpload(behavior) {
     }
 
 
+}
+//Se define para check el valor del slider de section 2 WIP
+let optionType = "";
+
+
+//Funcionalidad para definir las opciones disponibles en section 2.
+//Los equipos pueden ser de un minimo de 2 participantes.
+//Tener variable con que opciones quedarian desequilibrados los equipos.
+function createOptions() {
+    let playerL = players.length;
+
+
+
+}
+
+
+//Generar lista de options en base a numero de equipos
+function generateByTeamNumber(len) {
+    //Alamcenamos las opciones en un array auxiliar
+    let auxArrOptions = [];
+    for (let index = 2; index < len; index++) {
+        //Verificamos que los equipos sean de minimo 2 participantes
+        if (len / index < 2) {
+            break;
+        }
+        auxArrOptions.push(generateOptionHTML(index));
+    }
+    return auxArrOptions;
+
+}
+//Generar lista de options en base a numero de players
+function generateByPlayerNumber(len) {
+    let auxArrOptions = [];
+    //Los equipos pueden ser minimo de dos participantes, por lo que hacemos len.lenght/2 en el for-
+    for (let index = 2; index <= len.length / 2; index++) {
+        auxArrOptions.push(generateOptionHTML(index))
+    }
+    return auxArrOptions;
+}
+
+//Funcion para generar elemento HTML de options.
+function generateOptionHTML(num) {
+    let divElement = document.createElement("div");
+    divElement.classList.add("options");
+    divElement.innerHTML(num);
+    return divElement;
 }
