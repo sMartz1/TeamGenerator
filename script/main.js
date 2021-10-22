3b1b71555d22f988a552771fa23d7c82ac27b9d6
miguelCidPaz <miguel.cid.paz@gmail.com>
2021-10-22 22:21:00 +0200

    Randomizador listo, nose como meter mano a esas opciones

--- script/main.js
+++ script/main.js
@@ -47,7 +47,7 @@ function changeDom() {
             s1.classList.remove("noVisibility");
             break;
 
-            //Section 2
+        //Section 2
         case 1:
             //Actualizamos opciones siempre que se vaya a visualizar section2
             createOptions();
@@ -59,7 +59,7 @@ function changeDom() {
             }
             s2.classList.remove("noVisibility");
             break;
-            //Section 3
+        //Section 3
         case 2:
             if (!s1.classList.contains("noVisibility")) {
                 s1.classList.add("noVisibility");
@@ -83,7 +83,7 @@ function changeDom() {
 let valores = document.getElementsByClassName("text");
 //De esta manera detectamos el evento y lo recogemos en e para comprobar el key code
 //Aqui el Enter en este elemento añadira un nuevo player
-valores[0].addEventListener("keydown", function(e) {
+valores[0].addEventListener("keydown", function (e) {
     if (e.keyCode === 13) {
         addPlayer();
     }
@@ -101,20 +101,20 @@ function getPlayersByFile(e) {
     //Usamos file reader para acceder al archivo
     let reader = new FileReader();
     //Un controlador para el evento load. Este evento se activa cada vez que la operación de lectura se ha completado satisfactoriamente.
-    reader.onload = function(e) {
-            //en .result encontramos la string resultante de leer el archivo.
-            let content = e.target.result;
-            //Parto por saltos de linea los nombres para recibirlos como array
-            let arrGenerado = content.split(/\r?\n/g);
-            //Tenemos la variable arrGenerado con los players del .txt para su futuro uso.
-            //De momento dejamos en log el array
-            for (let i = 0; i < arrGenerado.length; i++) {
-                addPlayer(arrGenerado[i]);
-
-            }
+    reader.onload = function (e) {
+        //en .result encontramos la string resultante de leer el archivo.
+        let content = e.target.result;
+        //Parto por saltos de linea los nombres para recibirlos como array
+        let arrGenerado = content.split(/\r?\n/g);
+        //Tenemos la variable arrGenerado con los players del .txt para su futuro uso.
+        //De momento dejamos en log el array
+        for (let i = 0; i < arrGenerado.length; i++) {
+            addPlayer(arrGenerado[i]);
 
         }
-        //Se le pasa archivo a leer, si lo lee correctamente haria trigger en el .onload definido anteriormente.
+
+    }
+    //Se le pasa archivo a leer, si lo lee correctamente haria trigger en el .onload definido anteriormente.
     reader.readAsText(file);
     document.getElementById("inputF").value = "";
 }
@@ -131,26 +131,29 @@ function addPlayer(aux = "") {
     if (players.length < 1) {
         playerBoards[0].innerText = "";
     }
-    let valor = "";
+    let value = "";
     if (aux.length == 0) {
 
-        valor = valores[0].value;
+        value = valores[0].value;
 
     } else {
 
-        valor = aux;
+        value = aux;
     }
     //Recibimos el valor del input y lo introducimos a players
-
     //Comprobamos el tamaño del nombre para evitar missclick
-    if (valor.length < 2) {
+    if (value.length < 2) {
         return 'Nombre no valido'
     }
-    players.push(valor);
+    players.push(value);
+
+    drawPlayer(value, 0);
 
     //IMPORTANTE modifico creacion de elementos en base a nuevo diseño.
     //Necesaria estructura <div class="player"><p>$NAME</p><span>X</span></div>
+}
 
+function drawPlayer(value, option) {
     //Se crea div contenedor de la card
     let container = document.createElement('div');
     container.classList.add("player");
@@ -158,8 +161,8 @@ function addPlayer(aux = "") {
     //Se crea P para contener el nombre.
     let player = document.createElement('p');
     //Convertimos el valor a un textNode y creamos una nueva etiqueta para juntar ambos
-    valor = document.createTextNode(valor);
-    player.appendChild(valor);
+    value = document.createTextNode(value);
+    player.appendChild(value);
     //Añadimos atributos a nuestras etiquetas
     player.setAttribute("onclick", "modifyPlayer(this)");
     //player.setAttribute("class", "mod"); Dejo comentado por si necesitamos añadir clase en p en un futuro
@@ -173,8 +176,13 @@ function addPlayer(aux = "") {
     container.appendChild(player);
     container.appendChild(close);
 
-    //Agregamos el elemento al div boxText de la section 1
-    playerBoards[0].appendChild(container);
+    //Si option viene a 0 dibujara el nombre en el section 1
+    //Si viene a 1 devolvera el jugador dibujado para meterlo en la seleccion de teams
+    if (option == 0) {
+        playerBoards[0].appendChild(container);
+    } else if (option == 1) {
+        return container;
+    }
 }
 
 //Recoge el valor, lo busca en el array y lo sobreescribe mas tarde
@@ -189,7 +197,7 @@ function modifyPlayer(player) {
         if (player.isContentEditable) {
 
             //Evento que detectara si el usuario pulsa ENTER
-            player.addEventListener("keydown", function(e) {
+            player.addEventListener("keydown", function (e) {
                 if (e.keyCode === 13) {
                     player.removeAttribute("contentEditable")
                     valor = player.textContent;
@@ -198,7 +206,7 @@ function modifyPlayer(player) {
             })
 
             //Evento que controlara el valor si el elemento pierde el foco
-            player.addEventListener('blur', function(e) {
+            player.addEventListener('blur', function (e) {
                 player.addEventListener
                 player.removeAttribute("contentEditable")
                 valor = player.textContent;
@@ -298,6 +306,7 @@ function generateOptionHTML(num) {
     let divElement = document.createElement("div");
     divElement.classList.add("options");
     divElement.innerHTML = num;
+    divElement.setAttribute('onclick', 'probador()')
     return divElement;
 }
 
@@ -334,4 +343,44 @@ function sliderChange() {
         selectPlayer.classList.remove("noVisibility");
         sliderTarget.classList.remove("active-slider");
     }
+}
+
+
+//Pruebas funciones randomizer for section 3
+
+let options = document.getElementsByClassName('options');
+
+options.addEventListener('click', 'probador')
+
+//No deberia provocar retrasos
+function generateRandomArr() {
+    let randomArr = [];
+    while (randomArr.length < players.length) {
+        let random = Math.floor(Math.random() * players.length);
+        if (!randomArr.includes(random)) {
+            randomArr.push(random);
+        }
+    }
+    return randomArr;
+}
+
+//Devuelve nuevo arr con los players randomizados
+function randomTeams() {
+    let randoms = generateRandomArr();
+    let teams = [];
+
+    for (let i = 0; i < players.length; i++) {
+        teams.push(players[randoms[i]])
+    }
+
+    return teams;
+}
+
+function probador() {
+    console.log('Evento logrado');
+}
+
+function eventos() {
+    let optionsForTeams = document.getElementById('select-team').children;
+    console.log(optionsForTeams[0])
 }
