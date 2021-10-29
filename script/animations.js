636c808f85d64ac06f458eba000ffad7d15c6650
Sebastian M <ra3gan1991@gmail.com>
2021-10-29 18:20:45 +0200

    Acabo animacion de primera carga, seria posible reutilizar @keyframe logo-tic

--- script/animations.js
+++ script/animations.js
@@ -1,8 +1,9 @@
 //Evento en carga de pagina
 window.addEventListener("load", e => {
+    let main = document.getElementsByTagName("main")[0];
+    let modalWelcome = document.getElementsByClassName("modal-welcome")[0];
+    setTimeout(() => modalWelcome.remove(), 3000);
+    setTimeout(() => main.classList.remove("no-visibility"), 3000);
 
-    let s1 = document.getElementById("section1");
-    let main = document.getElementsByTagName("main");
-    setTimeout(() => main[0].classList.remove("no-visibility"), 2000);
-    setTimeout(() => s1.classList.remove("no-visibility"), 4000);
+
 })
